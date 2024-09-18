cc400600927e4bbd87dfe69c9872d0a7b4d00dc4
Brian Alonso VV <21045148@alumno.utc.edu.mx>
2024-09-18 14:17:48 -0600

    CAMBIO -> Enlaces del sidebar dinamicos - Se incluyo 'Font Awesome' a la plantilla 'admin.blade.php' - Se agrego la funcionalidad para mostrar cabeceras y enlaces del sidebar de forma dinamica

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/e8ac16af77.js" crossorigin="anonymous"></script>
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
