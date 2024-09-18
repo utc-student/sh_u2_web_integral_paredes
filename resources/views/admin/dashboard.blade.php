787dad817fcc096e87293628904afe889875a976
Brian Alonso VV <21045148@alumno.utc.edu.mx>
2024-09-18 12:17:51 -0600

    CAMBIO -> Creacion de la plantilla del administrador - Se creo un componente para la plantilla de administrador 'php artisan make:component AdminLayout' - Se devuelve la vista 'admin.dashboard' al acceder a la ruta admin, misma que extiende del componente recien creado. - Se separaron la barra de navegacion y el sidebar de la plantilla, para agregarlos como componentes individuales a traves de un include

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    Hola Mundo
+</x-admin-layout>
--- routes/admin.php
+++ routes/admin.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Admin Panel";
+    return view('admin.dashboard');
 })->name("dashboard");
