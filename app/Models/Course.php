9b03894283cd85ca23001b74e39416a567ce5b6a
Brian Alonso VV <21045148@alumno.utc.edu.mx>
2024-10-09 15:00:16 -0600

    CAMBIO -> Relaciones 1:M en Course - Se hicieron los metodos para acceder a la relacion 1:M de las llaves foraneas

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,19 +10,39 @@ class Course extends Model
     use HasFactory;
 
     protected $fillable = [
-        'title', 
-        'slug', 
-        'summary', 
-        'description', 
-        'status', 
-        'image_path', 
-        'video_path', 
-        'welcome_message', 
-        'goodbye_message', 
-        'observation', 
-        'user_id', 
-        'level_id', 
-        'category_id', 
+        'title',
+        'slug',
+        'summary',
+        'description',
+        'status',
+        'image_path',
+        'video_path',
+        'welcome_message',
+        'goodbye_message',
+        'observation',
+        'user_id',
+        'level_id',
+        'category_id',
         'price_id'
     ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function price()
+    {
+        return $this->belongsTo(Price::class);
+    }
 }
