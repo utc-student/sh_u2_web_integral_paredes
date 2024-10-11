dc8e7c1f7784378069dfaf49773813a89f41197d
Brian Alonso VV <21045148@alumno.utc.edu.mx>
2024-10-11 14:19:30 -0600

    CAMBIO -> CRUD cursos - Se creo un controlador para la creacion de cursos, mismo que se relaciono con el modelo Course. - Se ceraron las vista necesarias para cada accion del crud (edit, show, create, index)

--- routes/instructor.php
+++ routes/instructor.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Instructor\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('instructor.dashboard');
 });
+
+/* Cursos */
+Route::resource('courses', CourseController::class);
